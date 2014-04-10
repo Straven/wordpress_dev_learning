0ac094d1238ae18240731d39a169c8a1e02c3095
Straven <darthavalon@gmail.com>
2014-04-10 16:26:29 +0300

    tabs page

--- wp-content/themes/twentyfourteen/tabs.php
+++ wp-content/themes/twentyfourteen/tabs.php
@@ -7,14 +7,48 @@ get_header(); ?>
         <div id="content" class="site-content" role="main">
 
             <div id="tabs">
+
                 <ul>
-                    <li><a href="#tabs-1">First Post</a></li>
-                    <li><a href="#tabs-2">Second Post</a></li>
-                    <li><a href="#tabs-3">Third Post</a></li>
+                    <li><a href="#tabs-1">First Tab</a></li>
+                    <li><a href="#tabs-2">Second Tab</a></li>
+                    <li><a href="#tabs-3">Third Tab</a></li>
                 </ul>
-                <div id="tabs-1">There will be first post</div>
-                <div id="tabs-2">There will be second post</div>
-                <div id="tabs-3">There will be third post</div>
+                <div id="tabs-1">
+                    <?php
+                    $args = array(
+                        'post_type' => 'any',
+                        'posts_per_page' => 1,
+                    );
+                    $wp_query = new WP_Query($args);
+                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php the_content(); ?></p>
+                    <?php endwhile; ?>
+                </div>
+                <div id="tabs-2">
+                    <?php
+                    $args = array(
+                        'post_type' => 'post',
+                        'posts_per_page' => 1,
+                    );
+                    $wp_query = new WP_Query($args);
+                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php the_content(); ?></p>
+                    <?php endwhile; ?>
+                </div>
+                <div id="tabs-3">
+                    <?php
+                    $args = array(
+                        'post_type' => 'page',
+                        'posts_per_page' => 1,
+                    );
+                    $wp_query = new WP_Query($args);
+                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <p><?php the_content(); ?></p>
+                    <?php endwhile; ?>
+                </div>
             </div>
 
         </div>
