42fb02c107025df61f2b1467af66ce13a6b20629
Straven <darthavalon@gmail.com>
2014-04-10 17:25:02 +0300

    update for tabs

--- wp-content/themes/twentyfourteen/tabs.php
+++ wp-content/themes/twentyfourteen/tabs.php
@@ -8,44 +8,62 @@ get_header(); ?>
 
             <div id="tabs">
 
+                <?php
+                    $args_tab1 = array(
+                        'post_type' => 'post',
+                        'category_name' => 'bez-rubriki',
+                        'posts_per_page' => 1
+                    );
+                    $args_tab2 = array(
+                        'post_type' => 'post',
+                        'category_name' => 'shops',
+                        'posts_per_page' => 1
+                    );
+                    $args_tab3 = array(
+                        'post_type' => 'post',
+                        'category_name' => 'prosto-tak',
+                        'posts_per_page' => 1
+                    );
+                    $wp_query_tab1 = new WP_Query($args_tab1);
+                    $wp_query_tab2 = new WP_Query($args_tab2);
+                    $wp_query_tab3 = new WP_Query($args_tab3);
+                ?>
+
                 <ul>
-                    <li><a href="#tabs-1">First Tab</a></li>
-                    <li><a href="#tabs-2">Second Tab</a></li>
-                    <li><a href="#tabs-3">Third Tab</a></li>
+                    <li>
+                        <?php
+                        while ($wp_query_tab1->have_posts()) : $wp_query_tab1->the_post(); ?>
+                            <a href="#tabs-1"><?php the_title(); ?></a>
+                        <?php endwhile; ?>
+                    </li>
+                    <li>
+                        <?php
+                        while ($wp_query_tab2->have_posts()) : $wp_query_tab2->the_post(); ?>
+                            <a href="#tabs-2"><?php the_title(); ?></a>
+                        <?php endwhile; ?>
+                    </li>
+                    <li>
+                        <?php
+                        while ($wp_query_tab3->have_posts()) : $wp_query_tab3->the_post(); ?>
+                            <a href="#tabs-3"><?php the_title(); ?></a>
+                        <?php endwhile; ?>
+                    </li>
                 </ul>
                 <div id="tabs-1">
                     <?php
-                    $args = array(
-                        'post_type' => 'any',
-                        'posts_per_page' => 1,
-                    );
-                    $wp_query = new WP_Query($args);
-                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
+                    while ($wp_query_tab1->have_posts()) : $wp_query_tab1->the_post(); ?>
                         <p><?php the_content(); ?></p>
                     <?php endwhile; ?>
                 </div>
                 <div id="tabs-2">
                     <?php
-                    $args = array(
-                        'post_type' => 'post',
-                        'posts_per_page' => 1,
-                    );
-                    $wp_query = new WP_Query($args);
-                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
+                    while ($wp_query_tab2->have_posts()) : $wp_query_tab2->the_post(); ?>
                         <p><?php the_content(); ?></p>
                     <?php endwhile; ?>
                 </div>
                 <div id="tabs-3">
                     <?php
-                    $args = array(
-                        'post_type' => 'page',
-                        'posts_per_page' => 1,
-                    );
-                    $wp_query = new WP_Query($args);
-                    while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
-                        <h2><?php the_title(); ?></h2>
+                    while ($wp_query_tab3->have_posts()) : $wp_query_tab3->the_post(); ?>
                         <p><?php the_content(); ?></p>
                     <?php endwhile; ?>
                 </div>
