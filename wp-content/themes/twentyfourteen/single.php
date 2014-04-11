e344652ae89fe026008cd40fa87a275f9cd90927
Straven <darthavalon@gmail.com>
2014-04-11 12:02:46 +0300

    done with tabs

--- wp-content/themes/twentyfourteen/single.php
+++ wp-content/themes/twentyfourteen/single.php
@@ -7,7 +7,7 @@
  * @since Twenty Fourteen 1.0
  */
 
-get_header(); ?>
+//get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -20,21 +20,20 @@ get_header(); ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					the_content();
 
 					// Previous/next post navigation.
-					twentyfourteen_post_nav();
+					/*twentyfourteen_post_nav();*/
 
 					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
+					/*if ( comments_open() || get_comments_number() ) {
 						comments_template();
-					}
+					}*/
 				endwhile;
 			?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
 <?php
-get_sidebar( 'content' );
-get_sidebar();
-get_footer();
+//get_sidebar( 'content' );
+//get_sidebar();
+//get_footer();
